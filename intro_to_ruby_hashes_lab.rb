6b7728f3ba2c294c3452c0bff789f9d46c2e14b9
Alfonso Pruneda-Suarez <alfonso.pru@outlook.com>
2019-09-09 23:56:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,7 +7,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  hash = {railroads: {pieces: 4, rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200}, names: {reading_railroad: {}, pennsylvania_railroad: {}, third_name: 3, fourth_name: 4}}}
+  hash = {railroads: {pieces: 4, rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200}, names: {reading_railroad: {}, pennsylvania_railroad: {}, b_and_o_railroad: {}, fourth_name: {}}}}
 end
 
 def monopoly_with_fourth_tier
